4910d6c28cb4b29e69b6a6bda490939d7a96242b
Juuunyeok <junyeok333@gmail.com>
2026-01-06 17:36:37 +0900

    feat: add analyzer module for article processing

---
+++ src/analyzer/analyzer.controller.ts
@@ -0,0 +1,12 @@
+import { Controller } from '@nestjs/common';
+import { AnalyzerService } from './analyzer.service';
+
+@Controller('analyzer')
+export class AnalyzerController {
+  constructor(private readonly analyzerService: AnalyzerService) {}
+}
+
+
+
+
+
---
+++ src/analyzer/analyzer.util.ts
@@ -0,0 +1,6 @@
+// Analyzer utility functions
+
+
+
+
+
